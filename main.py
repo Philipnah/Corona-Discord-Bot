a85936730489ac914cadae6c220253ae0cf7f0e0
Philipnah <Philip@han.dk>
2020-03-30 19:09:51 +0200

    made my code more DRY

--- main.py
+++ main.py
@@ -4,8 +4,6 @@ from bs4 import BeautifulSoup
 import discord
 import os
 
-# from details import token
-
 token = os.environ.get("S3_KEY")
 
 # ------------- start data gathering ---------------------- #
@@ -46,31 +44,13 @@ client = discord.Client()
 async def on_message(message):
 
     # id = client.get_guild(idclient)
-    if message.content == "-corona news":
+    if message.content == "-corona news" or message.content == "-covid19 news" or message.content == "-sars cov2 news":
         embed = discord.Embed(title="Corona News", color=0x00ff00)
         embed.add_field(name="Smittede", value=data[0] + "er infekterede.", inline=False)
         embed.add_field(name="Døde", value=data[1] + " er døde.", inline=False)
         embed.add_field(name="Raske", value=data[2] + " er raske igen.", inline=False)
-        embed.add_field(name="", value=update, inline=False)
-        embed.add_field(name="Source", value=webpage, inline=False)
-        await message.channel.send(embed=embed)
- 
-    if message.content == "-covid19 news":
-        embed = discord.Embed(title="Corona News", color=0x00ff00)
-        embed.add_field(name="Smittede", value=data[0] + "er infekterede.", inline=False)
-        embed.add_field(name="Døde", value=data[1] + " er døde.", inline=False)
-        embed.add_field(name="Raske", value=data[2] + " er raske igen.", inline=False)
-        embed.add_field(name="", value=update, inline=False)
-        embed.add_field(name="Source", value=webpage, inline=False)
-        await message.channel.send(embed=embed)
-
-    if message.content == "-sars cov2 news":
-        embed = discord.Embed(title="Corona News", color=0x00ff00)
-        embed.add_field(name="Smittede", value=data[0] + "er infekterede.", inline=False)
-        embed.add_field(name="Døde", value=data[1] + " er døde.", inline=False)
-        embed.add_field(name="Raske", value=data[2] + " er raske igen.", inline=False)
-        embed.add_field(name="", value=update, inline=False)
-        embed.add_field(name="Source", value=webpage, inline=False)
+        embed.add_field(name="Update", value=update, inline=False)
+        embed.set_footer(text=webpage)
         await message.channel.send(embed=embed)
 
 
