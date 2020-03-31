ce7e97a0088ee2327c82916ec4a1eda216ce773e
Philipnah <Philip@han.dk>
2020-03-31 09:15:14 +0200

    Added continuous updates

--- main.py
+++ main.py
@@ -5,36 +5,39 @@ import discord
 import os
 
 token = os.environ.get("S3_KEY")
+webpage = "https://www.worldometers.info/coronavirus/"
 
 # ------------- start data gathering ---------------------- #
-data = []
+def DataGather():
+    data = []
 
-webpage = "https://www.worldometers.info/coronavirus/"
-datapage = requests.get(webpage)
+    datapage = requests.get(webpage)
 
-soup = BeautifulSoup(datapage.text, "lxml")
+    soup = BeautifulSoup(datapage.text, "lxml")
 
-coronaData = soup.find_all(class_="maincounter-number")
+    coronaData = soup.find_all(class_="maincounter-number")
 
-def splicer():
-     n = 0
-     while n <= 2:
-          coronaCases = str(coronaData[n]).split(">")
-          coronaCases = coronaCases[2].split("<")
-          coronaCases = coronaCases[0].replace(",", ".")
+    def splicer():
+        n = 0
+        while n <= 2:
+            coronaCases = str(coronaData[n]).split(">")
+            coronaCases = coronaCases[2].split("<")
+            coronaCases = coronaCases[0].replace(",", ".")
 
-          data.append(coronaCases)
-          n += 1
+            data.append(coronaCases)
+            n += 1
 
 
-splicer()
+    splicer()
 
-print(data)
+    print(data)
 
-coronaUpdate = soup.find("div", {"class": "content-inner"})
-update = coronaUpdate.findAll("div")[1].getText()
+    coronaUpdate = soup.find("div", {"class": "content-inner"})
+    update = coronaUpdate.findAll("div")[1].getText()
+    data.append(update)
 
-print(update)
+    print(update)
+    return data
 
 # ------------- end data gathering ---------------------- #
 
@@ -42,14 +45,14 @@ client = discord.Client()
 
 @client.event
 async def on_message(message):
+    data = DataGather()
 
-    # id = client.get_guild(idclient)
     if message.content == "-corona news" or message.content == "-covid19 news" or message.content == "-sars cov2 news":
         embed = discord.Embed(title="Corona News", color=0x00ff00)
         embed.add_field(name="Smittede", value=data[0] + "er infekterede.", inline=False)
         embed.add_field(name="Døde", value=data[1] + " er døde.", inline=False)
         embed.add_field(name="Raske", value=data[2] + " er raske igen.", inline=False)
-        embed.add_field(name="Update", value=update, inline=False)
+        embed.add_field(name="Update", value=data[3], inline=False)
         embed.set_footer(text=webpage)
         await message.channel.send(embed=embed)
 
