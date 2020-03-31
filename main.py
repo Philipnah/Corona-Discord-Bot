3008b68fae1ad4fc9871b3e8b5617fcfe085f127
Philipnah <Philip@han.dk>
2020-03-31 09:24:24 +0200

    fixed data gather

--- main.py
+++ main.py
@@ -45,9 +45,9 @@ client = discord.Client()
 
 @client.event
 async def on_message(message):
-    data = DataGather()
 
     if message.content == "-corona news" or message.content == "-covid19 news" or message.content == "-sars cov2 news":
+        data = DataGather()
         embed = discord.Embed(title="Corona News", color=0x00ff00)
         embed.add_field(name="Smittede", value=data[0] + "er infekterede.", inline=False)
         embed.add_field(name="Døde", value=data[1] + " er døde.", inline=False)
